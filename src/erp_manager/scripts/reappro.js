d59c402475b93d9504e3807265928115ae8372e4
montaru andy <andy.montaru@etu.univ-cotedazur.fr>
2024-03-20 10:14:51 +0100

    design + bouton confirmer/refuser

--- src/erp_manager/scripts/reappro.js
+++ src/erp_manager/scripts/reappro.js
@@ -7,25 +7,52 @@ function onLoad() {
 	let addButton = document.getElementById("addButton")
 	let validateButton = document.getElementById("validateButton")
 
+	// les champs
+	let productArea = document.getElementById("productChoice")
+	let quantityArea = document.getElementById("quantity")
+
 	//ajouter les listeners
 	addButton.addEventListener('click',addBasket)
 	validateButton.addEventListener('click',addInProgress)
-	setInterval(updateInProgress, 1000)
+	setInterval(updateInProgress, 100)
+	productArea.addEventListener('keyup',verifyTextArea)
+	quantityArea.addEventListener('keyup',verifyTextArea)
+
+	verifyTextArea()
 
 }
 
+
+// faire en sorte de recuperer les fournisseur et les met en option
+function getSupplier(){	// TODO
+
+}
+
+
 // faire en sorte de griser le bouton tant que les champs ne sont pas remplis
+function verifyTextArea(){
+	let addButton = document.getElementById("addButton")
+	let productArea = document.getElementById("productChoice")
+
+	// TODO later, verifier si le produit existe
+
+	if (productArea.value != ""){
+		addButton.disabled = false
+	}
+	else addButton.disabled = true
+}
 
 
+// on ajoute au panier quand on clique sur le bouton ajouter
 function addBasket(){
 
 	let product = document.getElementById("productChoice")
 	let quantity = document.getElementById("quantity")
-	let supplier = document.getElementById("Supplier")
 	
+	let quantityInt = 1;
 
 	// on verifie si tous les champs sont remplis
-	if (product.value == "" || quantity.value == "") return
+	if (product.value == "") return
 
 	// on verifie que quantity est un nombre
 	if (isNaN(quantity.value)){
@@ -33,11 +60,16 @@ function addBasket(){
 		return
 	} 
 	else quantity.style.background = "white"
+	
+	// on verifie si quantite est vide
+	if (quantity.value != "") quantityInt = parseInt(quantity.value)
 
 	// on creer l'element en question
-	let newProductName = document.createTextNode(product.value+": ")
+	let newProductName = document.createElement("span")
+	newProductName.innerHTML = product.value+" "
 	newProductName.className = "productName"
-	let newProductPrice = document.createTextNode(quantity.value * /*TODO prix du produit*/ 2+" €")
+	let newProductPrice = document.createElement("span")
+	newProductPrice.innerText = quantityInt * /*TODO prix du produit*/ 2+" €"
 	newProductPrice.className = "productPrice"
 
 	let newProduct = document.createElement("div")
@@ -56,8 +88,10 @@ function addBasket(){
 	product.focus()
 
 	updateTotal()
+	verifyTextArea()
 }
 
+// faire en sorte d'actualiser le total a chaque element rajouter ou enlever
 function updateTotal(){
 	// prendre les elements du panier
 	let basket = document.getElementById("basketItems")
@@ -81,15 +115,28 @@ function updateTotal(){
 	total.innerText = "Total: "+somme+" €"
 }
 
+
+// on ajoute a "En cours" a chaque fois fois qu'on appuis sur le bouton valider
 function addInProgress(){
 
 	let basket = document.getElementById("basketItems")
 	let inProgressItems = document.getElementById("inProgressItems")
 
+
+	let textPrice = basket.childNodes[0].lastChild.textContent.trim("€")
+	let priceProduct = parseInt(textPrice)
+
+
+	// une date dans le future
+	let now = new Date()
+	let time = now.getTime()+1000*priceProduct
+
+
 	// on rajoute une duree a l'element
-	let newProductTime = document.createTextNode(10/*TODO*/+"s")
+	let newProductTime = document.createTextNode( /*TODO*/+"s")
 	newProductTime.className = "productTime"
 	basket.childNodes[0].appendChild(newProductTime)
+	basket.childNodes[0].classList.add(""+time)
 
 	// on copy tous les elements dans le panier dans le inProgress
 	inProgressItems.appendChild(basket.childNodes[0])
@@ -103,19 +150,26 @@ function addInProgress(){
 }
 
 
+// on actualise les temps restant des livraisons
 function updateInProgress(){
 
 	let inProgressItems = document.getElementById("inProgressItems")
 
 	//actualiser le nombre de jours restant avant livraison
 	for (const child of inProgressItems.childNodes){
-		let textTime = child.lastChild.textContent.trim("s")
 
-		let timeProduct = parseInt(textTime)
+		// doit calculer le temps restant avant date de livraison
+		let time = child.classList[1]
+		let now = new Date()
 
-		child.lastChild.textContent = timeProduct-1+"s"
+		let diff = parseInt(time) - now.getTime()
 
-		if (timeProduct <= 0){
+		if (diff >= 1000*60*60*24) child.lastChild.textContent = parseInt(diff / (1000 * 60 * 60 *24))+"j"
+		else if (diff >= 1000*60*60)child.lastChild.textContent = parseInt(diff / (1000 * 60 * 60)) +"h"
+		else if (diff >= 1000*60)child.lastChild.textContent = parseInt(diff / (1000 * 60)) +"min"
+		else child.lastChild.textContent = parseInt(diff / (1000)) +"s"
+
+		if (diff <= 0){
 			child.lastChild.textContent = ""
 			addToConfirm(child)
 		} 
@@ -123,6 +177,8 @@ function updateInProgress(){
 
 }
 
+
+// on ajoute a "A confirmer" une fois le temps ecoule
 function addToConfirm(node){
 	let toConfirmItems = document.getElementById("toConfirmItems")
 
@@ -130,6 +186,7 @@ function addToConfirm(node){
 	addButtonConfirm(node)
 }
 
+// une fois transfere dans "A confirmer" on ajoute les bontons au produit pour confirmer
 function addButtonConfirm(node){
 
 	// on creer les deux boutons 
@@ -142,21 +199,29 @@ function addButtonConfirm(node){
 	refuteButton.textContent = "-"	// TODO changer par image
 
 	// on rajoute les events
-	confirmButton.addEventListener('click',confirmArrival)
-	refuteButton.addEventListener('click',refuteArrival)
+	confirmButton.addEventListener('click',() => {
+		let parentDirect = confirmButton.parentNode
+		let grandParent = parentDirect.parentNode
+		grandParent.removeChild(parentDirect)
+		console.log("confirmation de livraison du produit")
+	})
+	refuteButton.addEventListener('click',() => {
+		let parentDirect = confirmButton.parentNode
+		let grandParent = parentDirect.parentNode
+		grandParent.removeChild(parentDirect)
+		console.log("non confirmation de livraison du produit")
+	})
 
-	// on les rajoute a la node
-	node.appendChild(confirmButton)
-	node.appendChild(refuteButton)
-}
+	let divButton = document.createElement("div")
 
-function confirmArrival(){ // TODO
+	// on les rajoute a l'element
+	divButton.appendChild(confirmButton)
+	divButton.appendChild(refuteButton)
+	
+	// on rajoute l'element a la node
+	node.appendChild(divButton)
 
-}
-
-function refuteArrival(){  // TODO
-
-}
+} 
 
 
 window.onLoad = onLoad();
