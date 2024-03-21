791090b2ddb552bbdad8e771c16e23983f4eaa4e
moncada jeremy <jeremy.moncada@etu.unice.fr>
2024-03-21 11:28:33 +0100

    push en cachichou

--- src/erp_cash_desk/scripts/header.js
+++ src/erp_cash_desk/scripts/header.js
@@ -3,8 +3,7 @@ function updateCurrentTime() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    const currentTimeString = `${hours}:${minutes}:${seconds}`;
+    const currentTimeString = `${hours} : ${minutes}`;
     currentTimeElement.textContent = currentTimeString;
 }
   
