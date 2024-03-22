2785dae2a0fd0b5608907d4fe0cedde1ff412181
Nicolas Vetroff <nicolas.vetroff@etu.unice.fr>
2024-03-22 11:49:47 +0100

    changement port

--- src/erp_manager/scripts/selection_affichage.js
+++ src/erp_manager/scripts/selection_affichage.js
@@ -1,6 +1,6 @@
 let selection = document.getElementById('slct')
 
 selection.addEventListener("change", () => {
-    if(selection.options[selection.selectedIndex].text == "Caisse") window.location = 'http://localhost:8888/src/erp_cash_desk/employee.html'
-    else window.location = 'http://localhost:8888/src/erp_manager/manager.html'
+    if(selection.options[selection.selectedIndex].text == "Caisse") window.location = 'http://localhost:5500/src/erp_cash_desk/employee.html'
+    else window.location = 'http://localhost:5500/src/erp_manager/manager.html'
 })
