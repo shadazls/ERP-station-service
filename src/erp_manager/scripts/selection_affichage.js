acd7efff8d9def0a6fe7299f7d902f174c07f79a
as105237 <shad.azuelos@etu.univ-cotedazur.fr>
2024-03-24 13:47:48 +0100

    Update ERP and Caisse

--- src/erp_manager/scripts/selection_affichage.js
+++
@@ -1,6 +0,0 @@
-let selection = document.getElementById('slct')
-
-selection.addEventListener("change", () => {
-    if(selection.options[selection.selectedIndex].text == "Caisse") window.location = 'http://localhost:5500/src/erp_cash_desk/employee.html'
-    else window.location = 'http://localhost:5500/src/erp_manager/manager.html'
-})
