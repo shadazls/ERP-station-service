a071a08c6d4a6eace376266c6af9cc4d1831771f
as105237 <shad.azuelos@etu.univ-cotedazur.fr>
2024-03-27 09:21:26 +0100

    Drag and drop fixes

--- src/erp_manager/scripts/common/drag-and-drop.js
+++ src/erp_manager/scripts/common/drag-and-drop.js
@@ -1,14 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const containers = document.querySelectorAll('.draggable');
-    console.log("test1")
-    containers.forEach(container => {
-        new Draggable.Droppable(container, {
-            draggable: '.draggable',
-            delay: 200, // Facultatif : délai avant de commencer le déplacement
-            mirror: {
-                constrainDimensions: true // Facultatif : limiter la dimension du miroir à celle du conteneur
-            }
+// drag-and-drop.js
+
+// Fonction pour sauvegarder l'ordre des éléments dans le stockage local
+function saveSortOrder(containerId) {
+    const container = document.getElementById(containerId);
+    const items = container.querySelectorAll('.sortable');
+    const order = Array.from(items).map(item => item.id);
+    localStorage.setItem(containerId, JSON.stringify(order));
+}
+
+// Fonction pour restaurer l'ordre des éléments depuis le stockage local
+function restoreSortOrder(containerId) {
+    const container = document.getElementById(containerId);
+    const savedOrder = JSON.parse(localStorage.getItem(containerId));
+    if (savedOrder) {
+        savedOrder.forEach(itemId => {
+            const item = document.getElementById(itemId);
+            container.appendChild(item);
         });
-        console.log("test2")
+    }
+}
+
+// Initialiser SortableJS pour chaque container avec swap: true
+Sortable.create(document.getElementById('manager-container-top'), { group: 'container-group', swap: true });
+Sortable.create(document.getElementById('manager-container-center'), { group: 'container-group', swap: true });
+Sortable.create(document.getElementById('manager-container-bottom'), { group: 'container-group', swap: true });
+
+// Ajouter un événement onEnd pour sauvegarder l'ordre des éléments après le glisser-déposer
+[document.getElementById('manager-container-top'), document.getElementById('manager-container-center'), document.getElementById('manager-container-bottom')].forEach(container => {
+    container.addEventListener('sortupdate', function (evt) {
+        saveSortOrder(container.id);
     });
+});
+
+// Restaurer l'ordre des éléments après le chargement de tous les éléments dans le DOM
+window.addEventListener('load', function () {
+    restoreSortOrder('manager-container-top');
+    restoreSortOrder('manager-container-center');
+    restoreSortOrder('manager-container-bottom');
 });
