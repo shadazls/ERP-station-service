9560b8ca8b9f94e737724ce226d21849f695ceb0
Nicolas Vetroff <nicolas.vetroff@etu.unice.fr>
2024-03-27 15:32:53 +0100

    link back pump

--- src/erp_manager/scripts/pump.js
+++ src/erp_manager/scripts/pump.js
@@ -1,12 +1,31 @@
-let status_open = document.querySelectorAll(".status-open")
-let status_close = document.querySelectorAll(".status-close")
+for(let i = 1; i < 7; i++) {
+    let pump = document.getElementsByClassName("hvr-outline-out")
+    url = "https://api.fuelsync.hertinox.fr/pumps/getpump.php?idPump=" + i
 
-for (let elem of status_open) {
-    elem.style.color = "green";
-    elem.innerHTML = "Ouverte"
+    let xhr = new XMLHttpRequest()
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const response = JSON.parse(this.responseText);
+            console.log(response.isClose)
+            if(response.isClose == false) {
+                pump[i-1].getElementsByClassName("status")[0].classList.add("status-close")
+                pump[i-1].getElementsByClassName("status")[0].innerHTML = "Fermée"
+                pump[i-1].getElementsByClassName("status")[0].style.color = "red"
+            } else {
+                pump[i-1].getElementsByClassName("status")[0].classList.add("status-open")
+                pump[i-1].getElementsByClassName("status")[0].innerHTML = "Ouverte"
+                pump[i-1].getElementsByClassName("status")[0].style.color = "green"
+            }
+        } else {
+        // La requête a échoué
+            console.error("Erreur de requête: " + this.status);
+        }
+    }
+    xhr.open("GET", url, true)
+    xhr.send()
 }
 
-for (let elem of status_close) {
-    elem.style.color = "red";
-    elem.innerHTML = "Fermée"
-}
+
+
+
