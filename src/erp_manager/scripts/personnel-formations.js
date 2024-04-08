1069e8a53ca2ce67ba0341a596acfa13be9fde52
Nicolas Vetroff <nicolas.vetroff@etu.unice.fr>
2024-04-08 23:48:31 +0200

    forma

--- src/erp_manager/scripts/personnel-formations.js
+++ src/erp_manager/scripts/personnel-formations.js
@@ -36,33 +36,52 @@ xhr.send();
 
 // -------------------------------------------------
 
+let dateLocale
+let formation
+
+// -------------------------------------------------
+
 // ICI Je devrai récupérer les valeurs des formations (nom, date) avec AJAX pour les utiliser (JSON)
 xhr.onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-        console.log("Récupération formations réussie...")
-        response_formation = this.response;
-    } else {
-        console.log("Récupération formations échouée...");
-    }
+    if(this.readyState == 4) {
+        if(this.status == 200) {
+            console.log("Récupération formations réussie...")
+            response_formation = this.response;
+
+            console.log(response_formation)
+
+            for(formation of response_formation) {
+                dateLocale = formation.dateFormation.toLocaleString('fr-FR', {
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'long'
+                })
+                liste_formation.innerHTML += "<div class='formation'><p><span class='name'>"+ formation.name +"</span> - <span class='date'>" + dateLocale + "</span></p></div>"
+            }
+            onClickFormation();
+        } else {
+            console.log("Récupération formations échouée...");
+        }
+    } 
 }
 
-xhr.open("POST", "https://url.fr/formations.php", true);
+xhr.open("GET", "https://api.fuelsync.hertinox.fr/formations/getallformations.php", true);
 xhr.responseType = "json";
 xhr.send();
 
-/*for(let formation of response_formation.formations) {
-    // TODO
-}*/
+// ---------------------------------------------------------
 
-for(let node of liste_formation.childNodes) {
-    node.addEventListener("click", () => {
-        console.log("Nouvelle selection de formation...")
-        if(node.classList.contains("formation-selected")) {
-            node.classList.remove("formation-selected")
-        } else {
-            node.classList.add("formation-selected")
-        }
-    })
+function onClickFormation() {
+    for(let node of liste_formation.childNodes) {
+        node.addEventListener("click", () => {
+            console.log("Nouvelle selection de formation...")
+            if(node.classList.contains("formation-selected")) {
+                node.classList.remove("formation-selected")
+            } else {
+                node.classList.add("formation-selected")
+            }
+        })
+    }
 }
 
 subscribeFormation.addEventListener("click", () => {
