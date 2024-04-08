9e41c513c365da7c6796bb4e29c498c0ae26079e
Nicolas Vetroff <nicolas.vetroff@etu.unice.fr>
2024-04-08 16:55:10 +0200

    commit

--- src/erp_manager/scripts/pump.js
+++ src/erp_manager/scripts/pump.js
@@ -5,21 +5,20 @@ for(let i = 1; i < 7; i++) {
     let xhr = new XMLHttpRequest()
 
     xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const response = JSON.parse(this.responseText);
-            console.log(response.isClose)
-            if(response.isClose == false) {
-                pump[i-1].getElementsByClassName("status")[0].classList.add("status-close")
-                pump[i-1].getElementsByClassName("status")[0].innerHTML = "Fermée"
-                pump[i-1].getElementsByClassName("status")[0].style.color = "red"
-            } else {
-                pump[i-1].getElementsByClassName("status")[0].classList.add("status-open")
-                pump[i-1].getElementsByClassName("status")[0].innerHTML = "Ouverte"
-                pump[i-1].getElementsByClassName("status")[0].style.color = "green"
+        if (this.readyState == 4) {
+            if(this.status == 200) {
+                const response = JSON.parse(this.responseText);
+                console.log(response.isClose)
+                if(response.isClose == false) {
+                    pump[i-1].getElementsByClassName("status")[0].classList.add("status-close")
+                    pump[i-1].getElementsByClassName("status")[0].innerHTML = "Fermée"
+                    pump[i-1].getElementsByClassName("status")[0].style.color = "red"
+                } else {
+                    pump[i-1].getElementsByClassName("status")[0].classList.add("status-open")
+                    pump[i-1].getElementsByClassName("status")[0].innerHTML = "Ouverte"
+                    pump[i-1].getElementsByClassName("status")[0].style.color = "green"
+                }
             }
-        } else {
-        // La requête a échoué
-            console.error("Erreur de requête: " + this.status);
         }
     }
     xhr.open("GET", url, true)
